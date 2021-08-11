afc719e57f1b7463364ef55c217b644dc161a3d4
satoshi-mrn <satoshi.kwn1018@gmail.com>
2021-08-11 22:00:44 +0900

    ARC061 C

---
+++ practice/src/FullSearch.java
@@ -0,0 +1,37 @@
+package practice.src;
+
+import java.util.Scanner;
+
+class FullSearch {
+  public static void main(String[] args) {
+    // System.out.println((3 >> 0) & 1);
+    // System.out.println(1 << 9);
+    solutionABC045C();
+  }
+
+  // Practice: ARC 061 C
+  // see: https://atcoder.jp/contests/arc061/tasks/arc061_a
+  private static void solutionABC045C() {
+    Scanner sc = new Scanner(System.in);
+    char[] S = sc.next().toCharArray();
+    long ans = 0;
+    for (int i = 0; i < (1 << (S.length - 1)); i++) {
+      long sum = 0;
+      long tmp = 0;
+      for (int j = 0; j < S.length; j++) {
+        int num = Character.getNumericValue(S[j]);
+        if (((i >> j) & 1) > 0) {
+          sum += num + tmp * 10;
+          tmp = 0;
+        } else {
+          tmp *= 10;
+          tmp += num;
+        }
+      }
+      ans += sum + tmp;
+      tmp = 0;
+    }
+    System.out.println(ans);
+    sc.close();
+  }
+}
