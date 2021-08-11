536f39c6f8e06debe3f5c3f28c70195e664e90a4
satoshi-mrn <satoshi.kwn1018@gmail.com>
2021-08-11 22:44:06 +0900

    ABC079 C

--- practice/src/FullSearch.java
+++ practice/src/FullSearch.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 class FullSearch {
   public static void main(String[] args) {
-    solutionARC061C();
+    // solutionARC061C();
+    solutionABC079C();
   }
 
   // Practice: ARC 061 C
@@ -31,5 +32,35 @@ class FullSearch {
     }
     System.out.println(ans);
     sc.close();
+  }
+
+  // Practice: ABC 079 C
+  // see: https://atcoder.jp/contests/abc079/tasks/abc079_c
+  private static void solutionABC079C() {
+    Scanner sc = new Scanner(System.in);
+    char[] ABCD = sc.next().toCharArray();
+    sc.close();
+
+    final int expected = 7;
+    for (int i = 0; i < (1 << (ABCD.length - 1)); i++) {
+      int sum = Character.getNumericValue(ABCD[0]);
+      StringBuilder sb = new StringBuilder();
+      sb.append(Character.getNumericValue(ABCD[0]));
+      for (int j = 0; j < ABCD.length - 1; j++) {
+        int num = Character.getNumericValue(ABCD[j + 1]);
+        if (((i >> j) & 1) > 0) {
+          sum += num;
+          sb.append("+").append(num);
+        } else {
+          sum -= num;
+          sb.append("-").append(num);
+        }
+      }
+      if (sum == expected) {
+        sb.append("=7");
+        System.out.println(sb.toString());
+        return;
+      }
+    }
   }
 }
