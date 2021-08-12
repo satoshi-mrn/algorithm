bf00c9063d68ac0acb7ca0c93ee2a86d5f55dd51
satoshi-mrn <satoshi.kwn1018@gmail.com>
2021-08-12 12:01:46 +0900

    fix solutionARC061C

--- practice/src/FullSearch.java
+++ practice/src/FullSearch.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 class FullSearch {
   public static void main(String[] args) {
-    // solutionARC061C();
-    solutionABC079C();
+    solutionARC061C();
+    // solutionABC079C();
   }
 
   // Practice: ARC 061 C
@@ -15,20 +15,17 @@ class FullSearch {
     char[] S = sc.next().toCharArray();
     long ans = 0;
     for (int i = 0; i < (1 << (S.length - 1)); i++) {
-      long sum = 0;
-      long tmp = 0;
-      for (int j = 0; j < S.length; j++) {
-        int num = Character.getNumericValue(S[j]);
+      long tmp = Character.getNumericValue(S[0]);
+      for (int j = 0; j < S.length - 1; j++) {
         if (((i >> j) & 1) > 0) {
-          sum += num + tmp * 10;
-          tmp = 0;
+          ans += tmp;
+          tmp = Character.getNumericValue(S[j + 1]);
         } else {
           tmp *= 10;
-          tmp += num;
+          tmp += Character.getNumericValue(S[j + 1]);
         }
       }
-      ans += sum + tmp;
-      tmp = 0;
+      ans += tmp;
     }
     System.out.println(ans);
     sc.close();
